70e15c4d461c662c8524733eeabc1b82fb398493
AlerChen <applealerchen@icloud.com>
2020-08-09 12:09:57 +0800

    code InsertionSort of optimize

--- code/03-Insertion-Sort/src/com/company/InsertionSort.java
+++ code/03-Insertion-Sort/src/com/company/InsertionSort.java
@@ -25,28 +25,68 @@ public class InsertionSort {
     /*
 
     插入排序优化:
-        每次的交换 -> 直接赋值
-        减少程序操作的次数
+        每次的交换 -> 直接赋值(减少程序操作的次数:swap次数:数组寻址的次数)
 
+        1.先记录当前处理的数字
+        2.符合条件则将符合条件的数字向后平移一位,覆盖原有数字
+        3.内循环结束,将当前处理的数字赋值给最后一次判断位数字
+
+        3,5,2,7,1
+        i = 0
         temp = 3
+
         3,5,2,7,1
+        i = 1
         temp = 5
-        min = 3
+
         3,5,2,7,1
+        i = 2
         temp = 2
-        3,5,2,7,1
+        -> 3,5,5,7,1 符合条件数字向后平移一位,覆盖被判断位(arr[j] = arr[j-1]), j--
+        -> 3,3,5,7,1
+        -> arr[0] = temp;
+        -> 2,3,5,7,1
+
+        2,3,5,7,1
+        i = 3
+        temp = 7
+        -> 2,3,5,7,1
+
+        2,3,5,7,1
+        i = 4
+        temp = 1
+        ***  if arr[j-1] - temp > 0 ***
+        -> 2,3,5,7,7; j --
+        -> 2,3,5,5,7; j --
+        -> 2,3,3,5,7; j --
+        -> 2,2,3,5,7; j --
+        -> break;
+        -> arr[j] = temp;
+        -> 1,2,3,5,7
+
+
 
     */
     public static <E extends Comparable> void srot_optimize (E[] arr){
 
+        for (int i = 0; i < arr.length; i++) {
+            E temp = arr[i];
+            int j;
+            for (j = i; j -1 >= 0 && temp.compareTo(arr[j-1]) < 0; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[j] = temp;
         }
     }
-
     /*
+    InsertionSort:
     sortName: InsertionSort, n = 10000, time = 0.238406 s
     sortName: InsertionSort, n = 100000, time = 25.940432 s
+    srot_optimize:
+    sortName: InsertionSort_optimize, n = 10000, time = 0.194541 s
+    sortName: InsertionSort_optimize, n = 100000, time = 14.518286 s
     Time complexity : O(n^2)
-    */
+     */
 
     private static <E>void swap(E[] arr,int a,int b){
         E temp = arr[a];
@@ -81,7 +121,7 @@ public class InsertionSort {
         int[] dataArr = {10000, 100000};
         for (int data : dataArr) {
             Integer[] arr = ArrayGenerator.generateRandomArray(data, data);
-            SortingHelper.sortTest("InsertionSort", arr);
+            SortingHelper.sortTest("InsertionSort_optimize", arr);
         }
     }
     /*
