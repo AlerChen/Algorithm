c43bf903017e807229d344eec963b2bdd94a6b73
AlerChen <applealerchen@icloud.com>
2020-08-16 14:58:33 +0800

    Array generic data structures for test

--- code/04-Array/src/com/company/Main.java
+++ code/04-Array/src/com/company/Main.java
@@ -1,38 +1,46 @@
 package com.company;
 
-import com.company.d_Contain_Find_And_Remove.Array;
+import com.company.e_Generic_Data_Structures.Array;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Array arr = new Array(20);
+        Array<Integer> arr = new Array(20);
         for (int i = 0; i < 10; i++)
             arr.addLast(i);
         System.out.println(arr);
 
         arr.addLast(1);
+        System.out.println("arr.addLast(1);");
         System.out.println(arr);
 
         arr.addFirst(-1);
+        System.out.println("arr.addFirst(-1);");
         System.out.println(arr);
 
         int t = arr.get(1);
+        System.out.println("arr.get(1);");
         System.out.println(arr);
 
         arr.set(0,9);
+        System.out.println("arr.set(0,9);");
         System.out.println(arr);
 
         arr.removeLast();
+        System.out.println("arr.removeLast();");
         System.out.println(arr);
 
         arr.removeFirst();
+        System.out.println("arr.removeFirst();");
         System.out.println(arr);
 
         arr.remove(3);
+        System.out.println("arr.remove(3);");
         System.out.println(arr);
 
         arr.removeElement(7);
+        System.out.println("arr.removeElement(7);");
         System.out.println(arr);
 
 
