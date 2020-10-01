57cffb7deb0634e12dd6fb9ded567605d4381398
AlerChen <applealerchen@icloud.com>
2020-10-01 15:07:33 +0800

    homework of implement queue using stack with enqueue O(1), dequeue O(n), getFront O(1).

--- code/05-Stacks-and-Queues/src/com/company/i_LC232_implement_Queue_Using_Stacks/MyQueue_02.java
+++ code/05-Stacks-and-Queues/src/com/company/i_LC232_implement_Queue_Using_Stacks/MyQueue_02.java
@@ -1,49 +1,62 @@
 package com.company.i_LC232_implement_Queue_Using_Stacks;
+
+
 import java.util.Stack;
 
-
 /*
-* 作业优化: 使用队列实现栈, enqueue O(n), dequeue O(1), getFront O(1).
-* 设计思路: 将栈顶定义为队列头, enqueue,getFront 等同于 pop,peek.
-*         在enqueue时,将新元素插入栈顶即可.(使用辅助栈)
+* 作业优化: 使用栈实现队列, enqueue O(1), dequeue O(n), getFront O(1).
+* 设计思路: 定义栈顶为队列尾, 入队即push.
+*         出队时使用辅助栈push入原栈pop出的元素,再将辅助栈顶元素pop出,最后辅助栈内元素push回原栈即可.
+*         getFront使用属性记录,入队和出队时维护即可.
 * */
-public class MyQueue_01 <E>{
+public class MyQueue_02 <E>{
 
     /** Property */
     private Stack stack;
+    E front;
 
     /** Life Circle */
-    public MyQueue_01(){
+    public MyQueue_02() {
         stack = new Stack();
+        front = null;
     }
 
     /** Public Method */
     /*
-    * enqueue O(n)
+    * enqueue O(1)
     * */
     public void enqueue(E e){
-        Stack tempStack = new Stack();
-        while (!stack.isEmpty()){
-            tempStack.push(stack.pop());
-        }
         stack.push(e);
-        while (!tempStack.isEmpty()){
-            stack.push(tempStack.pop());
+        if (front == null){
+            front = e;
         }
     }
 
     /*
-    * dequeue O(1)
+    * dequeue O(n)
     * */
     public E dequeue(){
-        return (E) stack.pop();
+        if (stack.isEmpty()){
+            return null;
+        }
+        Stack tempStack = new Stack();
+        while (!stack.isEmpty()){
+            tempStack.push(stack.pop());
+        }
+        // 维护front元素
+        E targetE = (E) tempStack.pop();
+        front = (E) tempStack.peek();
+        while (!tempStack.isEmpty()){
+            stack.push(tempStack.pop());
+        }
+        return targetE;
     }
 
     /*
     * getFront O(1)
     * */
     public E getFront(){
-        return (E) stack.peek();
+        return front;
     }
 
     /*
@@ -55,15 +68,15 @@ public class MyQueue_01 <E>{
 
     @Override
     public String toString() {
-        return "MyQueue_01{" +
+        return "MyQueue_02{" +
                 "stack=" + stack +
+                ", front=" + front +
                 '}';
     }
 
     public static void main(String[] args) {
-
-        MyQueue_01<Integer> queue = new MyQueue_01();
-        System.out.println("MyQueue_01");
+        MyQueue_02<Integer> queue = new MyQueue_02();
+        System.out.println("MyQueue_02");
         System.out.println(queue);
         queue.enqueue(1);
         System.out.println("enqueue");
@@ -90,25 +103,24 @@ public class MyQueue_01 <E>{
 
         /*
 
-        MyQueue_01
-        MyQueue_01{stack=[]}
+        MyQueue_02
+        MyQueue_02{stack=[], front=null}
         enqueue
-        MyQueue_01{stack=[1]}
+        MyQueue_02{stack=[1], front=1}
         enqueue
-        MyQueue_01{stack=[2, 1]}
+        MyQueue_02{stack=[1, 2], front=1}
         enqueue
-        MyQueue_01{stack=[3, 2, 1]}
+        MyQueue_02{stack=[1, 2, 3], front=1}
         enqueue
-        MyQueue_01{stack=[4, 3, 2, 1]}
+        MyQueue_02{stack=[1, 2, 3, 4], front=1}
         dequeue
-        MyQueue_01{stack=[4, 3, 2]}
+        MyQueue_02{stack=[2, 3, 4], front=2}
         enqueue
-        MyQueue_01{stack=[5, 4, 3, 2]}
+        MyQueue_02{stack=[2, 3, 4, 5], front=2}
         getFront
-        MyQueue_01{stack=[5, 4, 3, 2]}
+        MyQueue_02{stack=[2, 3, 4, 5], front=2}
         front element is 2
 
         */
     }
-
 }
