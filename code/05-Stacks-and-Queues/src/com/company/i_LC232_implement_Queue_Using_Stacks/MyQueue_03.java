be4e2e445e751fed2da737f01e9207381e4362e1
AlerChen <applealerchen@icloud.com>
2020-10-01 17:49:11 +0800

    fix homework of implement queue using stack with enqueue O(1), dequeue O(n), average time complexity O(1), getFront O(1).

--- code/05-Stacks-and-Queues/src/com/company/i_LC232_implement_Queue_Using_Stacks/MyQueue_03.java
+++ code/05-Stacks-and-Queues/src/com/company/i_LC232_implement_Queue_Using_Stacks/MyQueue_03.java
@@ -26,7 +26,11 @@ public class MyQueue_03 <E>{
     * */
     public void enqueue(E e){
         stack1.push(e);
-        if (front == null) {
+        //if (front == null) {
+        //    front = e;
+        //}
+        // 防止pop空之后,不能维护front元素.
+        if (stack1.isEmpty()){
             front = e;
         }
     }
@@ -63,4 +67,62 @@ public class MyQueue_03 <E>{
         return stack1.isEmpty() && stack2.isEmpty();
     }
 
+    @Override
+    public String toString() {
+        return "MyQueue_03{" +
+                "stack1=" + stack1 +
+                ", stack2=" + stack2 +
+                ", front=" + front +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        MyQueue_03<Integer> queue = new MyQueue_03();
+        System.out.println("MyQueue_03");
+        System.out.println(queue);
+        queue.enqueue(1);
+        System.out.println("enqueue");
+        System.out.println(queue);
+        queue.enqueue(2);
+        System.out.println("enqueue");
+        System.out.println(queue);
+        queue.enqueue(3);
+        System.out.println("enqueue");
+        System.out.println(queue);
+        queue.enqueue(4);
+        System.out.println("enqueue");
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println("dequeue");
+        System.out.println(queue);
+        queue.enqueue(5);
+        System.out.println("enqueue");
+        System.out.println(queue);
+        Integer front = queue.getFront();
+        System.out.println("getFront");
+        System.out.println(queue);
+        System.out.println("front element is " + front);
+
+        /*
+
+        MyQueue_03
+        MyQueue_03{stack1=[], stack2=[], front=null}
+        enqueue
+        MyQueue_03{stack1=[1], stack2=[], front=null}
+        enqueue
+        MyQueue_03{stack1=[1, 2], stack2=[], front=null}
+        enqueue
+        MyQueue_03{stack1=[1, 2, 3], stack2=[], front=null}
+        enqueue
+        MyQueue_03{stack1=[1, 2, 3, 4], stack2=[], front=null}
+        dequeue
+        MyQueue_03{stack1=[], stack2=[4, 3, 2], front=null}
+        enqueue
+        MyQueue_03{stack1=[5], stack2=[4, 3, 2], front=null}
+        getFront
+        MyQueue_03{stack1=[5], stack2=[4, 3, 2], front=null}
+        front element is 2
+
+        */
+    }
 }
