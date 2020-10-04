1c3918f429159f657950d83f74990ef4beabce0d
AlerChen <applealerchen@icloud.com>
2020-10-04 16:44:44 +0800

    L203 remove elements in linked list

---
+++ code/07-Linked-List/src/i_L203_Remove_Elements_In_Linked_List/ListNode.java
@@ -0,0 +1,7 @@
+package i_L203_Remove_Elements_In_Linked_List;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){val = x;}
+}
---
+++ code/07-Linked-List/src/i_L203_Remove_Elements_In_Linked_List/Solution_01.java
@@ -0,0 +1,38 @@
+package i_L203_Remove_Elements_In_Linked_List;
+
+/**
+ *  L203: remove elements in linked list
+ *
+* */
+public class Solution_01 {
+
+    public ListNode removeElements(ListNode head, int val){
+
+        // 处理从头结点开始值相等的问题
+        while (head != null && head.val == val){
+            ListNode rmNode = head;
+            head = rmNode.next;
+            rmNode.next = null;
+        }
+
+        // 处理删除 值相等的头结点后 head为空问题
+        if (head == null)
+            return null;
+
+        ListNode preNode = head;
+        while (preNode.next != null){
+            if (preNode.next.val == val){
+                ListNode rmNode = preNode.next;
+                preNode.next = rmNode.next;
+                rmNode.next = null;
+                // 不做preNode后移操作
+            }else {
+                preNode = preNode.next;
+            }
+        }
+        return head;
+    }
+
+
+
+}
