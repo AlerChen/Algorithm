281089a2c63d5d29b9db7b8f1222483c21466df2
AlerChen <applealerchen@icloud.com>
2020-10-06 11:18:06 +0800

    fix L206 reverse linked list

--- code/07-Linked-List/src/k_L206_Reverse_Linked_List/Solution_01.java
+++ code/07-Linked-List/src/k_L206_Reverse_Linked_List/Solution_01.java
@@ -17,18 +17,43 @@ public class Solution_01 {
         next = null;
     }
 
+    /**
+     * 个人解题思路:
+     *    1.保存旧链表next节点
+     *      next = cur.next;
+     *    2.使新链表头节点pre成为旧链表当前头节点的next节点 (翻转步骤)
+     *      cur.next = pre;
+     *    3.使当前节点成为新链表头节点
+     *      pre = cur;
+     *    4.使旧链表next节点成为旧链表头结点
+     *      cur = next;
+     *    5. -> 1. while(cur != null)
+     * */
+
     public ListNode reverseLinkedList(ListNode head){
-        pre = null;
         cur = head;
-        next = head.next;
-        while (next != null){
+        while (cur != null){
+            next = cur.next;
             cur.next = pre;
             pre = cur;
             cur = next;
-            next = cur.next;
         }
-        return cur;
+        return pre;
     }
+
+//   根据老师思路解答错误
+//    public ListNode reverseLinkedList(ListNode head){
+//        pre = null;
+//        cur = head;
+//        next = head.next;
+//        while (cur != null){
+//            cur.next = pre;
+//            pre = cur;
+//            cur = next;
+//            next = cur.next;
+//        }
+//        return cur;
+//    }
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
@@ -38,5 +63,12 @@ public class Solution_01 {
         Solution_01 solution_01 = new Solution_01();
         ListNode reverseHead = solution_01.reverseLinkedList(head);
         System.out.println(reverseHead);
+
+        /**
+         Origin Linked List
+         1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> null
+         Reverse Linked List
+         7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> null
+         */
     }
 }
