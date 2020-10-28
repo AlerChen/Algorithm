8f848b4e0d0a863e252c4fbde0b3390132e032ae
alerchen <chengongda@xinyu100.com>
2020-10-28 09:03:13 +0800

    MergeSort test

---
+++ code/09-MergeSort/src/c_TestCode/MergeSort.java
@@ -0,0 +1,51 @@
+package c_TestCode;
+import java.util.Arrays;
+
+
+public class MergeSort<E>{
+
+    public static <E extends Comparable<E>> void sort(E[] arr){
+        sort(arr,0,arr.length-1);
+    }
+
+    public static <E extends Comparable<E>> void sort(E[]arr, int left, int right){
+        // basic question
+        if (left >= right){
+            return;
+        }
+        int mid = (left + right) / 2;
+        sort(arr,left,mid);
+        sort(arr,mid + 1,right);
+        merge(arr,left,mid,right);
+    }
+
+    public static <E extends Comparable<E>> void merge(E[]arr, int l, int mid, int r){
+        E[] tempArr = Arrays.copyOfRange(arr,l,r+1);
+        int i = l;
+        int j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid){
+                arr[k] = tempArr[j - l];
+                j++;
+            }else if (j > r){
+                arr[k] = tempArr[i - l];
+                i++;
+            }else if (tempArr[i - l].compareTo(tempArr[j - l]) <= 0){
+                arr[k] = tempArr[i - l];
+                i++;
+            }else {
+                arr[k] = tempArr[j - l];
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = 1000000;
+        Integer[] arr = ArrayGenerator.generateRandomArray(num,num);
+        SortingHelper.sortTest("MergeSort",arr);
+        /**
+         * sortName: MergeSort, n = 1000000, time = 0.355701 s
+         * */
+    }
+}
--- code/09-MergeSort/src/c_TestCode/SortingHelper.java
+++ code/09-MergeSort/src/c_TestCode/SortingHelper.java
@@ -20,21 +20,18 @@ public class SortingHelper {
         long startTime = System.nanoTime();
         if (sortName.equals("SelectionSort_Pattern")){
             SelectionSort_Pattern.sort(arr);
-        }
-        if (sortName.equals("SelectionSort_2")){
+        }else if (sortName.equals("SelectionSort_2")){
             SelectionSort_2.sort(arr);
-        }
-        if (sortName.equals("InsertionSort")){
+        }else if (sortName.equals("InsertionSort")){
             InsertionSort.sort(arr);
-        }
-        if (sortName.equals("InsertionSort_optimize")){
+        }else if (sortName.equals("InsertionSort_optimize")){
             InsertionSort.sort_optimize(arr);
-        }
-        if (sortName.equals("InsertionSort_reverse")){
+        }else if (sortName.equals("InsertionSort_reverse")){
             InsertionSort.sort_reverse(arr);
+        }else if (sortName.equals("MergeSort")){
+            MergeSort.sort(arr);
         }
 
-        
         long endTime = System.nanoTime();
         Double time = (endTime - startTime) / 1000000000.0;
         if (!isSorted(arr)){
